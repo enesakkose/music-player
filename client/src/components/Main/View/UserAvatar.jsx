3cbc1ea0ce94b8e15e96cb42122e336dda727100
enesakkose <ens.akkose@gmail.com>
2022-08-24 23:30:43 +0300

    user avatar button created

--- client/src/components/Main/View/UserAvatar.jsx
+++ client/src/components/Main/View/UserAvatar.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import '@/components/Main/View/UserAvatar.scss'
+import React, { useState } from 'react'
 import Icon from '@/components/Icon'
+import { Link } from 'react-router-dom'
+import { useClickOutside } from '@/hooks/useClickOutside'
+import '@/components/Main/View/UserAvatar.scss'
+
 
 function UserAvatar() {
+  const [ openAvatarMenu, setOpenAvatarMenu] = useState(false)
+
+  const domNode = useClickOutside(() => { 
+    setOpenAvatarMenu(false)
+  })
+
   return (
-    <button className='userAvatar'>
+    <div ref={domNode} className="userAvatar">
+      <button 
+        onClick={() => setOpenAvatarMenu(!openAvatarMenu)} className='avatar__btn'
+      >
         <Icon name='Avatar' size={28} style={{ color: 'gray'}}/>
         <span>Aaa</span>
-        <Icon name='Down'/>
-    </button>
+        <Icon name='Down' className={openAvatarMenu ? 'rotate' : null}/>
+      </button>
+
+      {openAvatarMenu && 
+        <div className="userAvatar__widgetMenu">
+          <ul>
+            <li>
+              <Link to='/'>
+                Hesap
+              </Link>
+            </li>
+            <li>
+              <Link to='/'>
+                Menu
+              </Link>
+            </li>
+            <li>
+              <button>Oturumu Kapat</button>
+            </li>
+          </ul>
+          
+        </div>
+      }
+    </div>
   )
 }
 
---
+++ client/src/hooks/useClickOutside.jsx
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from "react"
+
+export const useClickOutside = (menu) => {
+    const domNode = useRef()
+
+    useEffect(() => {
+        const maybeHandler = (e) => {
+            if(!domNode.current.contains(e.target)){
+                menu()
+            }
+        }
+
+        document.addEventListener("mousedown", maybeHandler)
+
+        return () => {
+            document.removeEventListener("mousedown", maybeHandler)
+        }
+    })
+
+    return domNode
+}
