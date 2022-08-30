9db83ea29ae61e9412ff9ce24d1320c86798329f
enesakkose <ens.akkose@gmail.com>
2022-08-30 23:24:38 +0300

    playlists listed

--- client/src/Pages/Collection/Playlists.jsx
+++ client/src/Pages/Collection/Playlists.jsx
@@ -2,49 +2,35 @@ import React from 'react'
 import EmptyPlaylist from '@/components/EmptyPlaylist'
 import FavoritesCard from '@/components/FavoritesCard'
 import InfoCard from '@/components/InfoCard'
+import { useSelector } from 'react-redux'
 import '@/Pages/Collection/Playlists.scss'
 
 function Playlists() {
+
+  const { playlists } = useSelector(state => state.playlist)
+  
   return (
     <div className='playlists contentSpacing'>
-        {/*<div className="playlists__empty">
-        <EmptyPlaylist 
+        {playlists.length === 0 
+        ? <div className="playlists__empty">
+            <EmptyPlaylist 
             title='Create your first playlist'
             text="It's easy, we'll help you."
         >
            <button className='emptyPlaylist-btn'>
              Create Playlist
            </button>
-        </EmptyPlaylist>  
-        </div>*/}
+            </EmptyPlaylist>  
+          </div>
+
+        : <div className="playlists__cards">
         <FavoritesCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
-        <InfoCard/>
+        {playlists.map((playlist) => (
+          <InfoCard key={playlist.id} playlist={playlist} />
+        ))}
+        </div>  
+      }
+        
     </div>
   )
 }
--- client/src/components/InfoCard.jsx
+++ client/src/components/InfoCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Icon from '@/components/Icon'
 import PlayBtn from '@/components/PlayBtn'
+import { Link } from 'react-router-dom'
 import '@/components/InfoCard.scss'
 
-function InfoCard() {
+function InfoCard({playlist}) {
+  
   return (
     <div className='infoCard'>
         <div className="infoCard__img">
@@ -11,11 +13,13 @@ function InfoCard() {
             <PlayBtn className='infoCard__img__btn'/>
         </div>
         <div className="infoCard__info">
-            <h4>My Playlist #1</h4>
+            <h4>{playlist.name}</h4>
             <span>By Aaa(username)</span>
         </div>
+        <Link to={`/playlist/${playlist.id}`} className='perde'></Link>
     </div>
   )
 }
-
 export default InfoCard
+
+//! A "perde" ELEMENT USED TO PREVENT THE PLAY BUTTON FROM BEING AFFECTED BY THE PUBLIC LINK
