1d51d629bd0b6da650d7c76a263cbfb98b9cfebf
enesakkose <ens.akkose@gmail.com>
2022-08-31 20:05:56 +0300

    created  useFocus hook

--- client/src/components/SearchInput.jsx
+++ client/src/components/SearchInput.jsx
@@ -1,22 +1,26 @@
 import React, { useState } from 'react'
+import { useFocus } from '@/hooks/useFocus'
 import Icon from '@/components/Icon'
+
 import '@/components/SearchInput.scss'
 
 function SearchInput() {
-
+  const inputRef = useFocus()
   const [ query, setQuery ] = useState('')
+  
 
   return (
       <form className='searchInput'>
         <label>
           <Icon className='searchIcon' name='Search' size={24}/>
-          <input 
+          <input
+          ref={inputRef}
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder='What do you want to listen to ?'
           />
-          {query && <button onClick={() => setQuery('')} className='closeBtn'>
+          {query && <button type='button' onClick={() => setQuery('')} className='closeBtn'>
             <Icon name='Close' size={24}/>
           </button>}
         </label>
