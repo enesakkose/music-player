723f100192ca9008aa3c6244fb5b187a4d5ecba0
enesakkose <ens.akkose@gmail.com>
2022-09-16 01:07:50 +0300

    data request created for routes

--- client/src/components/Main/View/ViewLayout.jsx
+++ client/src/components/Main/View/ViewLayout.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ViewLayoutHeader from '@/components/Main/View/ViewLayoutHeader'
+import { useDispatch } from 'react-redux'
+import { setSongs } from '@/store/song'
+import songData from '@/api/songs.json'
 import '@/components/Main/View/ViewLayout.scss'
 
+
 function ViewLayout({children}) {
+
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    dispatch(setSongs(songData))
+  }, [])
+
   return (
     <section className='viewLayout'>
         <ViewLayoutHeader/>
