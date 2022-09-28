2b0f9b04775bd1850a760f5c2850d92f4e708f8e
enesakkose <ens.akkose@gmail.com>
2022-09-29 02:24:29 +0300

    genre cards added

--- client/src/Pages/Search.jsx
+++ client/src/Pages/Search.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { GENRES } from '@/constants'
+import { Link } from 'react-router-dom'
+import '@/Pages/Search.scss'
 
 function Search() {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <h3>CATEGORIES</h3>
+      <div className="search__genres">
+        {GENRES.map((genre) => (
+          <Link 
+            key={genre.val} 
+            to={`/genre/${genre.val}`} 
+            className='search__genres__card'
+          >
+            <h2 className='search__genres__card__title'>
+              {genre.genre}
+            </h2>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
