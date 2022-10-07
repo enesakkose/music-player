7fc9359cfe453b22134ae0ba253140200fdaa7d1
enesakkose <ens.akkose@gmail.com>
2022-10-07 19:57:39 +0300

    package added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,9 +7,11 @@ import { store } from '@/store'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
-     <BrowserRouter>
-       <App />
-     </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
+  </React.StrictMode>
 )
