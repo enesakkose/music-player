8bef0672957e6cb0d926ede84b3eba2496ab27ad
enesakkose <ens.akkose@gmail.com>
2022-10-10 17:26:57 +0300

    playlistHeaderInPlaylist component created

---
+++ client/src/Pages/Playlist/PlaylistHeaderInPlaylist.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useClickOutside } from '@/hooks/useClickOutside'
+import { useSelector } from 'react-redux'
+import PlaylistHeader from '@/components/PlaylistHeader'
+import Icon from '@/components/Icon'
+import DropdownMenu from '@/components/DropdownMenu'
+import '@/Pages/Playlist/PlaylistHeaderInPlaylist.scss'
+
+function PlaylistHeaderInPlaylist({playlistId}) {
+
+  const { playlists } = useSelector(state => state.playlist)
+  const playlistName = playlists.find((playlist) => playlist.id === playlistId)
+  const [ open, setOpen ] = useState(false)
+  
+  const domNode = useClickOutside(() => { 
+    setOpen(false)
+  })
+
+  return (        
+    <PlaylistHeader className="playlistHeaderInPlaylist">
+      {/*//!  single classnames in child elements is coming from PlaylistHeader components//*/}
+      <div className="playlistHeaderInPlaylist__cover cover">
+        <Icon name='Music' size={75}/>
+        <span className="playlistHeaderInPlaylist__cover__action">
+          <Icon name='Pencil' size={48}/>
+          Choose Photo
+        </span>
+      </div>
+      <div ref={domNode} className="playlistHeaderInPlaylist__info info">
+        <h6>PLAYLIST</h6>
+        <h1>{playlistName?.name}</h1>
+        <h6 className='playlistHeaderInPlaylist__info__userName'>
+          <Link to='/'>
+            Aaa(username)
+          </Link>
+          <button onClick={() => setOpen(!open)} className="playlistAction-btn">
+            <Icon name='ThreeDots' size={32}/>
+          </button>
+          {open && 
+            <DropdownMenu  className='playlistActionMenu'>
+              <ul>
+                <li>
+                  <button>Edit Details</button>
+                </li>
+                <li>
+                  <button>Delete</button>
+                </li>
+              </ul>
+            </DropdownMenu>
+          }
+        </h6>
+      </div>
+    </PlaylistHeader>
+  )
+}
+
+export default PlaylistHeaderInPlaylist
