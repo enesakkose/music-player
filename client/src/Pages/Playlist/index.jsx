6f614f3df7439e0e7499c4c24b9053db6af1e20d
enesakkose <ens.akkose@gmail.com>
2022-10-17 22:06:01 +0300

    fix

--- client/src/Pages/Playlist/index.jsx
+++ client/src/Pages/Playlist/index.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import PlaylistHeaderInPlaylist from '@/Pages/Playlist/PlaylistHeaderInPlaylist'
-import PlaylistMain from '@/Pages/Playlist/Main'
+import PlaylistMain from '@/Pages/Playlist/PlaylistMain'
 import '@/Pages/Playlist/Playlist.scss'
 
 function Playlist() {
   const { playlistId } = useParams()
   const { playlist } = useSelector(state => state.playlist)
-  const bgColor = playlist[0]?.track?.images?.joecolor?.slice(18,24)
-  
+  const bgColor = playlist[0]?.track?.images?.joecolor?.slice(18, 24)
+
   return (
     <div key={playlistId} className='playlist'>
-      <PlaylistHeaderInPlaylist playlistId={playlistId} bgColor={bgColor}/>
-      <PlaylistMain playlistId={playlistId} bgColor={bgColor}/>  
+      <PlaylistHeaderInPlaylist playlistId={playlistId} bgColor={bgColor} />
+      <PlaylistMain playlistId={playlistId} bgColor={bgColor} />
     </div>
   )
 }
