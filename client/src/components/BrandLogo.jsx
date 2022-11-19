7f654c80e70b63f95573d479e4af93542cead59f
enesakkose <ens.akkose@gmail.com>
2022-11-19 02:24:02 +0200

    css fix and added className to brandLogo

--- client/src/components/BrandLogo.jsx
+++ client/src/components/BrandLogo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import clsx from 'clsx'
 import { Link } from 'react-router-dom'
 import Icon from '@/components/Icon'
 import '@/components/BrandLogo.scss'
 
-function BrandLogo({size}) {
+function BrandLogo({size, className}) {
   return (
-    <h3 className="brandLogo">
+    <h3 className={clsx('brandLogo', className)}>
       <Link to='/'>
         <Icon name='Logo' size={size}/>
         MUSIC
