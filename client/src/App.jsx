90e12d0a6a2fb90ce336817b2b03cd5636622545
enesakkose <ens.akkose@gmail.com>
2022-11-20 22:22:21 +0200

    error boundary added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import ErrorFallback from '@/components/ErrorFallback'
 import { routes } from '@/routes'
 import { useRoutes } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
+import { ErrorBoundary } from 'react-error-boundary'
 
 function App() {
   return (
     <div className="App">
-      {useRoutes(routes)}
+      <ErrorBoundary key={uuidv4()} FallbackComponent={ErrorFallback}>
+        {useRoutes(routes)}
+      </ErrorBoundary>
     </div>
   )
 }
--- client/src/Pages/404.jsx
+++ client/src/Pages/404.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import BrandLogo from '@/components/BrandLogo'
+import clsx from 'clsx'
 import { Link } from 'react-router-dom'
 import '@/Pages/404.scss'
 
-function App404() {
+function App404({errorMessage = 'Page Not Found', children, className}) {
   return (
-    <div className='app404'>
+    <div className={clsx('app404', className)}>
       <BrandLogo size={55}/>
-      <h2>Page Not Found</h2>
+      <h2>{errorMessage}</h2>
       <Link to='/' className='emptyFieldBtn'>Home</Link>
+      {children}
     </div>
   )
 }
