a122a9aa86a441687e3ad29925b0fd084a5323ba
enesakkose <ens.akkose@gmail.com>
2022-11-20 23:55:12 +0200

    error boundary location changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
-import ErrorFallback from '@/components/ErrorFallback'
+import React from 'react'
 import { routes } from '@/routes'
 import { useRoutes } from 'react-router-dom'
-import { v4 as uuidv4 } from 'uuid'
-import { ErrorBoundary } from 'react-error-boundary'
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary key={uuidv4()} FallbackComponent={ErrorFallback}>
-        {useRoutes(routes)}
-      </ErrorBoundary>
+      {useRoutes(routes)}
     </div>
   )
 }
--- client/src/components/MainLayout/index.jsx
+++ client/src/components/MainLayout/index.jsx
@@ -3,10 +3,14 @@ import Sidebar from '@/components/Sidebar'
 import Footer from '@/components/Footer'
 import ViewLayoutHeader from '@/components/View/ViewLayoutHeader'
 import Popups from '@/components/MainLayout/Popups'
-import { Outlet } from 'react-router-dom'
+import ErrorFallback from '@/components/ErrorFallback'
+import { Outlet, useLocation } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
 import '@/components/MainLayout/MainLayout.scss'
 
 function MainLayout() {
+  const location = useLocation()
+
   return (
     <>
       <section className='main'>
@@ -14,7 +18,9 @@ function MainLayout() {
         <Sidebar/>
         <div className='viewLayout'>
           <ViewLayoutHeader/>
-          <Outlet/>
+          <ErrorBoundary key={location.pathname} FallbackComponent={ErrorFallback}>
+            <Outlet/>
+          </ErrorBoundary>
         </div>
       </section>
       <Footer/>
