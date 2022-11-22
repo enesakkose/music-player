4e71970d16a3cd921cb088cf460855cbb0697762
enesakkose <ens.akkose@gmail.com>
2022-11-23 00:59:36 +0200

    playlistHighlight added and playlistInfoCard changes

--- client/src/components/PlaylistInfoCard.jsx
+++ client/src/components/PlaylistInfoCard.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import Icon from '@/components/Icon'
 import PlayBtn from '@/components/PlayBtn'
+import clsx from 'clsx'
+import { setCurrent, setCurrentSongs, playPause } from '@/store/player'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import '@/components/playlistInfoCard.scss'
 
-function PlaylistInfoCard({playlist, user}) {
+function PlaylistInfoCard({playlist, user = false}) {
+  const dispatch = useDispatch()
+  const { current, isPlaying } = useSelector(state => state.player)
   const validCoverImg = playlist.coverURL === null && playlist.addedSongs.length > 0
   const coverImage = playlist?.addedSongs[0]?.track?.images?.coverart
+  const haveSongs = playlist.addedSongs.some(song => song.id === current.key)
+  
+  const playInPlaylist = () => {
+    if(current.key !== playlist.addedSongs[0].id && haveSongs) 
+    return dispatch(playPause(!isPlaying))
+
+    dispatch(setCurrent(playlist.addedSongs[0].track))
+    dispatch(setCurrentSongs(playlist.addedSongs))
+
+    if(current.key === playlist.addedSongs[0].id) return dispatch(playPause(!isPlaying))
+    if(current.key !== playlist.addedSongs[0].id) return dispatch(playPause(true))
+  }
 
   return (
     <div className='playlistInfoCard'>
@@ -19,7 +36,11 @@ function PlaylistInfoCard({playlist, user}) {
               : <Icon name='Music' size={52}/>
             )
         }
-        <PlayBtn className='playlistInfoCard__img__btn'/>
+        {playlist.addedSongs.length > 0 && <PlayBtn
+          onClick={playInPlaylist}
+          playPause={isPlaying && haveSongs}
+          className={`playlistInfoCard__img__btn ${isPlaying && haveSongs ? 'showBtn': ''}`}
+        />}
       </div>
       <div className="playlistInfoCard__info">
         <h5>{playlist.name}</h5>
