318862dd33685345e5512c9890787fcde8f3b713
enesakkose <ens.akkose@gmail.com>
2022-11-30 21:02:13 +0200

    errorBoundary location changed

--- client/src/components/MainLayout/index.jsx
+++ client/src/components/MainLayout/index.jsx
@@ -4,27 +4,28 @@ import Footer from '@/components/Footer'
 import ViewLayoutHeader from '@/components/View/ViewLayoutHeader'
 import Popups from '@/components/MainLayout/Popups'
 import ErrorFallback from '@/components/ErrorFallback'
+import UnauthFooterBar from '@/components/Footer/UnauthFooterBar'
+import { useSelector } from 'react-redux'
 import { Outlet, useLocation } from 'react-router-dom'
 import { ErrorBoundary } from 'react-error-boundary'
 import '@/components/MainLayout/MainLayout.scss'
 
 function MainLayout() {
   const location = useLocation()
+  const { user } = useSelector(state => state.auth)
 
   return (
-    <>
+    <ErrorBoundary key={location.pathname} FallbackComponent={ErrorFallback}>
       <section className='main'>
         <Popups/>
         <Sidebar/>
         <div className='viewLayout'>
           <ViewLayoutHeader/>
-          <ErrorBoundary key={location.pathname} FallbackComponent={ErrorFallback}>
-            <Outlet/>
-          </ErrorBoundary>
+          <Outlet/>
         </div>
       </section>
-      <Footer/>
-    </>
+      {user ? <Footer/> : <UnauthFooterBar/>}
+    </ErrorBoundary>
   )
 }
 
