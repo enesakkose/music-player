2c038f57534f3ae28ab73a8f41bebd602904ae54
enesakkose <ens.akkose@gmail.com>
2022-11-30 14:09:59 +0200

    window reload added

--- client/src/Pages/404.jsx
+++ client/src/Pages/404.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import BrandLogo from '@/components/BrandLogo'
 import LightBtn from '@/components/LightBtn'
 import clsx from 'clsx'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import '@/Pages/404.scss'
 
 function App404({errorMessage = 'Page Not Found', children, className}) {
+  const navigate = useNavigate()
+  const refreshPage = () => {
+    navigate('/', {replace: true})
+    window.location.reload()
+  }
+  
   return (
     <div className={clsx('app404', className)}>
       <BrandLogo size={55}/>
       <h2>{errorMessage}</h2>
-      <Link to='/'>
-        <LightBtn text='Home'/>
-      </Link>
+      <LightBtn text='Home' onClick={refreshPage}/>
       {children}
     </div>
   )
