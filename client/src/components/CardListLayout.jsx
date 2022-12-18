6faa6ae62a04a21d211a6e3d5a197894c50f571e
enesakkose <ens.akkose@gmail.com>
2022-12-18 02:26:14 +0200

    state prop added

--- client/src/components/CardListLayout.jsx
+++ client/src/components/CardListLayout.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '@/components/CardListLayout.scss'
 
-function CardListLayout({ href, title, children }) {
+function CardListLayout({ href = true, title, children, ...props }) {
   return (
     <div className='cardListLayout'>
       <div className="cardListLayout__title">
         <h3>{title}</h3>
-        <Link to={href}>See All</Link>
+        {href && <Link to={href} state={{...props}}>See All</Link>}
       </div>
       <div className='cardListLayout__list'>
         {children}
