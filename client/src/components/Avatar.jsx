fcef154872bf48303330b2e31a7aaaeb69c64bb5
enesakkose <ens.akkose@gmail.com>
2022-12-19 02:46:38 +0200

    conditions changed

--- client/src/components/Avatar.jsx
+++ client/src/components/Avatar.jsx
@@ -3,10 +3,12 @@ import Icon from '@/components/Icon'
 import '@/components/Avatar.scss'
 
 function Avatar({src, size, ...props}) {
+  const a = src === null || src === ''
+
   return (
     <>
-      {src === null && <Icon name='Avatar' size={size}/>}
-      {src !== null && <img 
+      {a && <Icon name='Avatar' size={size}/>}
+      {!a && <img 
         src={src}
         referrerPolicy="no-referrer"
         style={{ width: `${size}` ,height: `${size}`, borderRadius: '50%', ...props}}
