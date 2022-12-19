ebd8cff896dcfb218978da31c303f18a7460eb28
enesakkose <ens.akkose@gmail.com>
2022-12-19 03:12:42 +0200

    optional chaining added

--- client/src/Pages/Profile/ProfileMain/index.jsx
+++ client/src/Pages/Profile/ProfileMain/index.jsx
@@ -3,7 +3,7 @@ import Favorites from '@/Pages/Profile/ProfileMain/Favorites'
 import '@/Pages/Profile/ProfileMain/ProfileMain.scss'
 
 function ProfileMain({profile}) {
-  const favLength = profile.favorites.length > 0
+  const favLength = profile?.favorites?.length > 0
 
   return (
     <div className='profileMain'>
