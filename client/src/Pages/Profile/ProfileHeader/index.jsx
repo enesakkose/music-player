b15cdb94d0acf1622b7f696552d434c802c93c43
enesakkose <ens.akkose@gmail.com>
2022-12-19 03:11:25 +0200

    user prop removed

--- client/src/Pages/Profile/ProfileHeader/index.jsx
+++ client/src/Pages/Profile/ProfileHeader/index.jsx
@@ -5,17 +5,17 @@ import ProfileHeaderBtns from '@/Pages/Profile/ProfileHeader/ProfileHeaderBtns'
 import { modal } from '@/utils'
 import '@/Pages/Profile/ProfileHeader/ProfileHeader.scss'
 
-function ProfileHeader({ profile, validProfile, user }) {
+function ProfileHeader({ profile, validProfile }) {
   return (
     <PlaylistHeader 
       className='profileHeader' 
       infoTitle='PROFILE' 
       infoHeader={profile.displayName}
       img={profile.photoURL === null || profile.photoURL === '' ? DefaultAvatar : profile.photoURL}
-      onClick={validProfile ? () => modal('UserInfoModal', user) : undefined}
+      onClick={validProfile ? () => modal('UserInfoModal') : undefined}
       validProfile={validProfile}
     >
-      <ProfileHeaderBtns profile={profile} validProfile={validProfile} user={user}/>
+      <ProfileHeaderBtns profile={profile} validProfile={validProfile}/>
     </PlaylistHeader>
   )
 }
