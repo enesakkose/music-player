8a4bb081a0e1c8decb7e3cd68a549ee3cc1d4f9b
enesakkose <ens.akkose@gmail.com>
2022-12-25 22:03:32 +0200

    user profile info changed with current info

--- client/src/components/Modal/Comment.jsx
+++ client/src/components/Modal/Comment.jsx
@@ -3,20 +3,29 @@ import Icon from '@/components/Icon'
 import Avatar from '@/components/Avatar'
 import { Link } from 'react-router-dom'
 import { useGetTime } from '@/hooks/useTimeConvert'
+import { useGetProfile } from '@/hooks/useGetProfile'
+import MyLoader from '@/components/Modal/Skeleton'
 import '@/components/Modal/Comment.scss'
 
 function Comment({comment}) {
   const time = useGetTime(comment.createdAt)
+  const profile = useGetProfile(comment.uid)
+  
+  if(profile === null) return
+
   return (
     <div className='comment'>
-      <Avatar src={comment.photoURL} size='32px'/>
-      <div className="comment__text">
-        <div className='comment__text__title'>
-          <Link to={`profile/${comment.uid}`}>@{comment.displayName}</Link>
-          <span>{time}</span>
+      
+        <Avatar src={profile?.photoURL} size='32px'/>
+        <div className="comment__text">
+          <div className='comment__text__title'>
+            <Link to={`profile/${comment.uid}`}>@{profile?.displayName}</Link>
+            <span>{time}</span>
+          </div>
+          <p className='comment__text__content'>{comment.comment}</p>
         </div>
-        <p className='comment__text__content'>{comment.comment}</p>
-      </div>
+      
+
     </div>
   )
 }
