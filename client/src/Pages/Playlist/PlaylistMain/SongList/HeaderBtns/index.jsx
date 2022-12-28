aeb3c6b5049bc7012b814a43337d5ac3297ace65
enesakkose <ens.akkose@gmail.com>
2022-12-28 22:47:24 +0200

    unAuthModal added

--- client/src/Pages/Playlist/PlaylistMain/SongList/HeaderBtns/index.jsx
+++ client/src/Pages/Playlist/PlaylistMain/SongList/HeaderBtns/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ActionBtns from '@/components/ActionBtns'
 import Icon from '@/components/Icon'
+import { useSelector } from 'react-redux'
 import { useNumberFormat } from '@/hooks/useTimeConvert'
 import { modal } from '@/utils'
 import '@/Pages/Playlist/PlaylistMain/SongList/HeaderBtns/HeaderBtns.scss'
 
 function HeaderBtns({ playlist }) {
+  const { user } = useSelector(state => state.auth)
   const onlyTracks = playlist.addedSongs.length > 0 && playlist.addedSongs.map((a => a.track))
 
   const commentModal = () => {
-    modal('CommentModal', playlist.id)
+    user
+    ? modal('CommentModal', playlist.id)
+    : modal('UnauthModal')
   }
 
   return (
