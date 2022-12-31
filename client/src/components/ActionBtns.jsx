e3249111dcc295772c4a8109042d05714da51e92
enesakkose <ens.akkose@gmail.com>
2022-12-31 15:35:51 +0200

    haveSongs changed inSongs

--- client/src/components/ActionBtns.jsx
+++ client/src/components/ActionBtns.jsx
@@ -8,14 +8,14 @@ import '@/components/ActionBtns.scss'
 function ActionBtns({findSongs, children, className}) {
   const { current, isPlaying } = useSelector(state => state.player)
   const { user } = useSelector(state => state.auth)
-  const haveSongs = findSongs.some(f => f.key === current.key)
+  const inSongs = findSongs.some(f => f.key === current.key)
 
   return (
     <div className={clsx('actionBtns', className)}>
       <PlayBtn
-        onClick={() => usePlaySong(findSongs, current, isPlaying, user)}
+        onClick={() => usePlaySong(findSongs, inSongs, current, isPlaying, user)}
         className='actionBtns__play'
-        playPause={isPlaying && haveSongs}
+        playPause={isPlaying && inSongs}
       />
       {children}
     </div>
