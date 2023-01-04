fe37094cef064369b1da1fe1a58cc4b494c83cfd
enesakkose <ens.akkose@gmail.com>
2023-01-04 16:58:45 +0200

    progressBar seperated from MusicPlayer

---
+++ client/src/components/Footer/ProgressBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import clsx from 'clsx'
+import CustomRange from '@/components/CustomRange'
+import { getTimeConvert } from '@/utils/number'
+import '@/components/Footer/ProgressBar.scss'
+
+
+function ProgressBar({ songTime, duration, mobile, onChange }) {
+  return (
+    <div className={clsx(mobile ? "mobileProgressBar" : "progressBar")}>
+      {!mobile && <span className='duration'>
+        {getTimeConvert(songTime)}
+      </span>}
+      <CustomRange
+        className='range'  
+        min={0} 
+        max={duration}
+        value={songTime} 
+        onChange={onChange}
+      />
+      {mobile && <div className="mobileDuration">
+        <span className='duration'>
+          {getTimeConvert(songTime)}
+        </span>
+        <span className='duration'>
+          {getTimeConvert(duration)}
+        </span>
+      </div>}
+      {!mobile && <span className='duration'>
+        {getTimeConvert(songTime)}
+      </span>}
+    </div>
+  )
+}
+
+export default ProgressBar
