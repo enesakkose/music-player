ba37ff0255a1b9c538ac423b0a787cf8a74c99e9
enesakkose <ens.akkose@gmail.com>
2023-01-07 20:41:55 +0200

    selection: typo fix popup and pagewrapper changes for mobile player: some css removed

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import MobilePlayer from '@/components/MobilePlayer'
 import Navbar from '@/components/Sidebar/Navbar'
+import { useSelector } from 'react-redux'
 import '@/components/Navbar/Navbar.scss'
 
 function MobileNavbar() {
+  const { user } = useSelector(state => state.auth)
+
   return (
     <div className='navbar'>
       <MobilePlayer/>
-      <Navbar/>
+      <Navbar user={user}/>
     </div> 
   )
 }
