9e3b7dcdb01124f9e9ccd7e97cd1d0782dfcc554
enesakkose <ens.akkose@gmail.com>
2023-01-10 01:50:35 +0300

    hook edited

--- client/src/hooks/useGetWindowSize.jsx
+++ client/src/hooks/useGetWindowSize.jsx
@@ -1,26 +1,41 @@
 import React, { useEffect, useState } from 'react'
 
 export const useGetWindowSize = () => {
-  const [screenType, setScreenType] = useState("INITIAL")
+  const isWindowClient = typeof window === "object"
+  const getBreakPoint = (windowWidth) => {
+    if (windowWidth) {
+      if (windowWidth < 480) {
+        return "SM"
+      } else if (windowWidth <= 1024) {
+        return "MD";
+      } else if (windowWidth <= 1200) {
+        return "LG"
+      } else {
+        return "XL"
+      }
+    } else {
+      return undefined
+    }
+  }
+  
+  const [screenType, setScreenType] = useState(
+    isWindowClient    
+    ? getBreakPoint(window.innerWidth)
+    : undefined
+  )
 
   useEffect(() => {
-    const updateWindowSize = () => {
-      if(window.innerWidth <= 640) setScreenType("SM")
-      if(window.innerWidth > 640) setScreenType("MD")
-      if(window.innerWidth > 768) setScreenType("LG")
-      if(window.innerWidth > 1024) setScreenType("XL")
-      if(window.innerWidth > 1536) setScreenType("INITIAL")
+    const handleSize = () => {
+      setScreenType(getBreakPoint(window.innerWidth))
     }
-    
-    window.addEventListener('resize', updateWindowSize)
 
-    updateWindowSize()
-    
-    return () => {
-      window.removeEventListener('resize', updateWindowSize)
+    if(isWindowClient) {
+      window.addEventListener('resize', handleSize)
+      
+      return () => window.removeEventListener('resize', handleSize)
     }
     
-  }, [window.innerWidth])
+  }, [isWindowClient, setScreenType])
 
   return screenType
 }
