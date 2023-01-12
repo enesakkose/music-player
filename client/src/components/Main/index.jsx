6c3e28c5c4fd043146b1a21ab4fb1b206c59e388
enesakkose <ens.akkose@gmail.com>
2023-01-12 18:14:44 +0300

    sidebar removed and size props send to viewLayout comp.

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Sidebar from '@/components/Sidebar'
 import ViewLayout from '@/components/View/Layout'
+import { getMobileTabletSize } from '@/utils/size'
 import '@/components/Main/Main.scss'
 
 function Main() {
+  const size = getMobileTabletSize()
+  
   return (
     <section className='main'>
-      <Sidebar />
-      <ViewLayout />
+      {!size && <Sidebar />}
+      <ViewLayout size={size}/>
     </section>
   )
 }
