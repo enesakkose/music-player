484e6ba6c2822ef675f15a7cae3f0b504b9ee90a
enesakkose <ens.akkose@gmail.com>
2023-01-20 22:03:06 +0300

    SongsTableHeader added

--- client/src/Pages/Genre/Main/List.jsx
+++ client/src/Pages/Genre/Main/List.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import TrackList from '@/components/TrackList'
+import SongsTableHeader from '@/components/SongsTableHeader'
 import Row from '@/components/TrackList/Row'
 import TrackListWrapper from '@/components/Wrappers/TrackListWrapper'
 import ActionBtns from '@/components/ActionBtns'
 import GradientBg from '@/components/GradientBg'
+import { getMobileTabletSize } from '@/utils/size'
 
 function List({ songs, song, title, bg }) {
+  const size = getMobileTabletSize()
+
   return (
     <TrackListWrapper className='mobileGenreList'>
       <ActionBtns
@@ -13,7 +17,8 @@ function List({ songs, song, title, bg }) {
         subtitle={song.subtitle}
         songLength={songs.length}
         findSongs={songs}
-      />
+        />
+      {!size && <SongsTableHeader/>}
       <TrackList>
         {songs.map((song, index) => (
           <Row
