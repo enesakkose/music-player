a46881d6730fd424c79b226b611677c2b9f892a4
enesakkose <ens.akkose@gmail.com>
2023-01-24 16:05:08 +0300

    addOrRemove func removed and addToPlaylistBtn comp and css aded

--- client/src/Pages/Playlist/PlaylistMain/Search/Result/List.jsx
+++ client/src/Pages/Playlist/PlaylistMain/Search/Result/List.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import Row from '@/components/TrackList/Row'
 import TrackList from '@/components/TrackList'
-import AddOrRemoveBtn from '@/Pages/Playlist/PlaylistMain/Search/Result/AddOrRemoveBtn'
-import { addToAddedSongs, removeFromAddedSongs } from '@/firebase/db'
+import LightBtn from '@/components/LightBtn'
+import { removeFromAddedSongs } from '@/firebase/db'
+import { addToPlaylist } from '@/utils/song'
 import '@/Pages/Playlist/PlaylistMain/Search/Result/List.scss'
 
 function List({ result, isSuccess, isFetching, playlist, show }) {
   const resultSong = show ? [] : result?.tracks?.hits?.slice(1,11)
-
-  const addOrRemove = (song) => {
-    const findAddedSongs = playlist.addedSongs.find(s => s.id === song.key)
-
-    findAddedSongs 
-    ? removeFromAddedSongs(playlist.id, findAddedSongs)
-      
-    : addToAddedSongs(playlist.id, {
-        id: song.key,
-        track: song,
-        createdAt: new Date().toISOString()
-      })
-  }
 
   return (
     <TrackList className='resultTrackList'>
@@ -29,12 +17,13 @@ function List({ result, isSuccess, isFetching, playlist, show }) {
           index={index}
           song={song.track}
           songs={resultSong}
+          actionBtns={false}
           className='resultTrackList__item'
         >
-          <AddOrRemoveBtn 
-            onClick={() => addOrRemove(song.track)}
-            id={song.track.key}
-            playlist={playlist}
+          <LightBtn
+            text='Add'
+            onClick={() => addToPlaylist(playlist.id, song.track)}
+            className='addToPlaylistBtn'
           />
         </Row>
       ))}
