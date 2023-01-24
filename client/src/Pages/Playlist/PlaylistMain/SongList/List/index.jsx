f881b9357a85215c44ba57bf544a2cd6e2764bca
enesakkose <ens.akkose@gmail.com>
2023-01-24 16:03:52 +0300

    CustomPlaylistDropdownItem comp added, css fix

--- client/src/Pages/Playlist/PlaylistMain/SongList/List/index.jsx
+++ client/src/Pages/Playlist/PlaylistMain/SongList/List/index.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Row from '@/components/TrackList/Row'
 import TrackList from '@/components/TrackList'
+import DropdownMenuItem from '@/components/DropdownMenu/DropdownMenuItem'
 import moment from 'moment'
+import { removeFromAddedSongs } from '@/firebase/db'
 import '@/Pages/Playlist/PlaylistMain/SongList/List/List.scss'
 
 function List({ playlist }) {
+
+  const CustomPlaylistDropdownItem = ({song}) => {
+    return(
+      <DropdownMenuItem 
+        text='Remove from playlist' 
+        onClick={() => removeFromAddedSongs(playlist.id, song)}
+      />
+    )
+  }
+
+  //This component only have customPlaylist
+
   return (
     <TrackList className='songList'>
       {playlist.addedSongs.map((song, index) => (
@@ -13,7 +27,8 @@ function List({ playlist }) {
             index={index}
             song={song.track}
             songs={playlist.addedSongs}
-            customPlaylist={true}
+            customPlaylist={<CustomPlaylistDropdownItem song={song}/>}
+            playlistId={playlist.id}
             className='songList__item'
           >
             <span>{moment(song.createdAt).fromNow()}</span>
