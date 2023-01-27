8645936035df9a6bdc801f8e0df64956128ee55f
enesakkose <ens.akkose@gmail.com>
2023-01-27 19:11:45 +0300

    CreatePlaylistBtn added

--- client/src/Pages/Collection/Playlists.jsx
+++ client/src/Pages/Collection/Playlists.jsx
@@ -3,31 +3,23 @@ import PageWrapper from '@/components/Wrappers/PageWrapper'
 import CardListWrapper from '@/components/Wrappers/CardListWrapper'
 import EmptyField from '@/components/EmptyField'
 import FavoritesCard from '@/components/FavoritesCard'
-import LightBtn from '@/components/LightBtn'
 import PlaylistInfoCard from '@/components/PlaylistInfoCard'
 import CreatePlaylistBtn from '@/components/CreatePlaylistBtn'
-import { addPlaylist } from '@/utils/playlist'
 import { getMobileTabletSize } from '@/utils/size'
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
 import '@/Pages/Collection/Playlists.scss'
 
 function Playlists() {
-  const navigate = useNavigate()
   const { playlists } = useSelector(state => state.playlist)
-  const { profile: { favorites, uid, displayName } } = useSelector(state => state.profiles)
+  const { profile: { favorites, displayName } } = useSelector(state => state.profiles)
   const size = getMobileTabletSize()
-
-  const handleAdd = () => {
-    addPlaylist(playlists, uid, navigate)
-  }
   
   return (
     <PageWrapper className='playlists'>
       <h3>Your Playlists</h3>
       {playlists.length === 0 
         ? <EmptyField icon='Music'>
-            <LightBtn onClick={handleAdd} text='Create Playlist'/>
+            <CreatePlaylistBtn size={42}/>
           </EmptyField>  
 
         : <CardListWrapper className="playlists__cards">
