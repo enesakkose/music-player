cfdd1cf61ed57827cf215e08e78a13fd1c81edd1
enesakkose <ens.akkose@gmail.com>
2023-01-30 22:53:54 +0300

    CreatePlaylistBtn

--- client/src/components/CreatePlaylistBtn/index.jsx
+++ client/src/components/CreatePlaylistBtn/index.jsx
@@ -3,7 +3,7 @@ import Icon from '@/components/Icon'
 import { useSelector } from 'react-redux'
 import { addPlaylist } from '@/utils/playlist'
 import { useNavigate } from 'react-router-dom'
-import '@/components/CreatePlaylistButton.scss'
+import styles from '@/components/CreatePlaylistButton/CreatePlaylistButton.module.scss'
 
 function CreatePlaylistBtn({ size }) {
   const navigate = useNavigate()
@@ -11,7 +11,7 @@ function CreatePlaylistBtn({ size }) {
   const { profile: { uid } } = useSelector(state => state.profiles)
 
   return (
-    <button onClick={() => addPlaylist(playlists, uid, navigate)} className='createPlaylistBtn'>
+    <button onClick={() => addPlaylist(playlists, uid, navigate)} className={styles.createPlaylistBtn}>
       <Icon name='add' size={size}/>
     </button>
   )
